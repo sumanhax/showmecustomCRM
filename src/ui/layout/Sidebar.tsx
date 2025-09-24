b322415c03f62f0f73aaf8b7f0470ba83406e71b
sumanhax <suman.hax@gmail.com>
2025-09-24 20:57:03 +0530

    msg

--- src/ui/layout/Sidebar.tsx
+++ src/ui/layout/Sidebar.tsx
@@ -98,7 +98,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-4 py-5 lg:py-[23px]">
-        <NavLink className="text-center w-full" to="/">
+        <NavLink className="text-center w-full" to="/crm-dashboard">
    
           {sidebarOpen ?
             <>
